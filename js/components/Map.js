4b5ee9b4e74a78e7816ef97ffd9770424528dbbf
Fryderyk Górski <fryderykgorski@gmail.com>
2020-04-05 17:42:35 +0200

    Added functioning map display while the data from API is still being fetched

--- js/components/Map.js
+++ js/components/Map.js
@@ -9,26 +9,42 @@ export default class Map extends Component {
 
   render() {
     return (
-      <LeafletMap center={this.props.center} zoom={this.props.zoom}>
+      <LeafletMap onClick={this.handleClick} center={this.props.center} zoom={this.props.zoom} >
         <TileLayer attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <DevPlans data={this.props.data} />
+
+        {this.props.data
+        ?
+        <LocalDevPlans data={this.props.data} />
+        :
+        null
+        }
+
       </LeafletMap>
     )
   }
 }
 
-class DevPlans extends Component {
+class LocalDevPlans extends Component {
   constructor(props) {
     super(props);
+  }
+
+
+  handlePolygonClick = (e) => {
+    console.log("Kliknięto poligon");
+    const polygon = this.ref.current;
+    console.log(polygon);
+    //console.log(polygon.props.polygonData.nazwa_planu);
   }
 
   render() {
     const polygons = this.props.data.features;
 
     const polygonsCoords = [];
+    let singlePolygonCoords;
 
     polygons.forEach( (polygon) => {
-      const singlePolygonCoords = [];
+      singlePolygonCoords = [];
       polygon.geometry.coordinates[0].forEach( (pointCoords) => {
         singlePolygonCoords.push(pointCoords.reverse())
       })
@@ -36,7 +52,18 @@ class DevPlans extends Component {
     })
 
     const polyRender = polygonsCoords.map( (polygonXY, index) => {
-      return <Polygon key={index} positions={polygonXY}/>
+      return(
+        <Polygon onClick={this.handlePolygonClick} ref="polygon" key={index} positions={polygonXY} polygonData={polygons[index].properties}>
+          <Popup>
+            <ul className="devPlan-popup">
+              <li>Nazwa planu: {polygons[index].properties.nazwa_planu}</li>
+              <li>Dzielnica: {polygons[index].properties.dzielnica}</li>
+              <li>Typ planu: {polygons[index].properties.typ_planu}</li>
+              <li>Data uchwalenia: {polygons[index].properties.data_uchwalenia}</li>
+            </ul>
+          </Popup>
+        </Polygon>
+      )
     })
 
     return(
