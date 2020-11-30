2a1e5227f3bc47848bd278ab1d3146bed54e1a20
Fryderyk Górski <fryderykgorski@gmail.com>
2020-11-30 23:17:09 +0100

    Resigned from React-Leaflet and prepared to transition project into OpenLayers library

--- src/App.js
+++ src/App.js
@@ -7,15 +7,6 @@ import Navigation from "./components/Navigation";
 import './App.scss';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lat: 52.23235,
-      lng: 21.00412,
-      zoom: 13,
-      data: false
-    }
-  }
 
   render() {
     return (
@@ -30,7 +21,7 @@ class App extends Component {
           </Route>
 
           <Route path="/mapa">
-            <Map center={this.state.lat, this.state.lng} zoom={this.state.zoom} data={this.state.data} />
+            
           </Route>
 
         </Switch>
