d92c011fd6505b7ffcd3137e2cd1ed4247e1b2f1
Fryderyk Górski <fryderykgorski@gmail.com>
2020-12-09 22:04:33 +0100

    Added vector layers fetching from API and vector layer display

--- src/components/Map/Layers/layerStyles.js
+++ src/components/Map/Layers/layerStyles.js
@@ -1,13 +1,40 @@
 import { Fill, Stroke, Style } from 'ol/style';
 
 let styles = {
-    "MultiPolygon": new Style({
+    "DevPlansPolygon": new Style({
         stroke: new Stroke({
             color: "blue",
             width: 1,
         }),
         fill: new Fill({
             color: "rgba(0, 0, 255, 0.1)",
+        }),
+    }),
+    "SinglePolygon": new Style({
+        stroke: new Stroke({
+            color: "red",
+            width: 1,
+        }),
+        fill: new Fill({
+            color: "rgba(100, 100, 255, 0.1)",
+        }),
+    }),
+    "MultiPolygon": new Style({
+        stroke: new Stroke({
+            color: "green",
+            width: 1,
+        }),
+        fill: new Fill({
+            color: "rgba(100, 100, 255, 0.1)",
+        }),
+    }),
+    "MixedPolygon": new Style({
+        stroke: new Stroke({
+            color: "purple",
+            width: 1,
+        }),
+        fill: new Fill({
+            color: "rgba(100, 100, 255, 0.1)",
         }),
     }),
 }
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -3,6 +3,7 @@ import MapProvider from './MapProvider';
 import Layers from './Layers/Layers';
 import BaseLayer from './Layers/BaseLayer';
 import VectorLayer from './Layers/VectorLayer';
+import { fetchDevPlans, fetchDevTypes } from '../../utilities/apiCalls';
 
 import styles from './Layers/layerStyles';
 import { fromLonLat, get } from 'ol/proj';
@@ -16,23 +17,38 @@ import ToggleLayerControl from './Controls/ToggleLayerControl';
 const Map = () => {
     const [center, setCenter] = useState([21.00412, 52.23235]);
     const [zoom, setZoom] = useState(13);
-    const [geoJSONData, setGeoJSONData] = useState(null);
+    const [devPlansData, setDevPlansData] = useState(null);
+    const [singleData, setSingleData] = useState(null);
+    const [multiData, setMultiData] = useState(null);
+    const [mixedData, setMixedData] = useState(null);
+
     const [showLayer, setShowLayer] = useState(true);
 
     useEffect(() => {
-        fetch("https://cors-anywhere.herokuapp.com/mapa.um.warszawa.pl/WebServices/ZasiegiPlanow/wgs84/findAll/")
-        .then(res => res.json())
-        .then(data => setGeoJSONData(data));
-    })
+        fetchDevPlans(setDevPlansData);
+        fetchDevTypes(setSingleData, setMultiData, setMixedData);
+    }, []);
 
     return (
         <MapProvider center={fromLonLat(center)} zoom={zoom}>
             <Layers>
                 <BaseLayer source={osm()} zIndex={0} />
 
-                {geoJSONData && <VectorLayer
-                    source={vector({ features: new GeoJSON().readFeatures(geoJSONData, { featureProjection: get('EPSG:3857') }) })}
+                {devPlansData && <VectorLayer
+                    source={vector({ features: new GeoJSON().readFeatures(devPlansData, { featureProjection: get('EPSG:3857') }) })}
+                    style={styles.DevPlansPolygon} />}
+
+                {singleData && <VectorLayer
+                    source={vector({ features: new GeoJSON().readFeatures(singleData, { featureProjection: get('EPSG:3857') }) })}
+                    style={styles.SinglePolygon} />}
+
+                {multiData && <VectorLayer
+                    source={vector({ features: new GeoJSON().readFeatures(multiData, { featureProjection: get('EPSG:3857') }) })}
                     style={styles.MultiPolygon} />}
+
+                {mixedData && <VectorLayer
+                    source={vector({ features: new GeoJSON().readFeatures(mixedData, { featureProjection: get('EPSG:3857') }) })}
+                    style={styles.MixedPolygon} />}
 
             </Layers>
             <Controls>
