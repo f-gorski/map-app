84e7839307b21a8af79d068efa58c10b639977ce
Fryderyk Górski <fryderykgorski@gmail.com>
2021-02-15 21:06:21 +0100

    feature: changed Basemap style to MapBox supplied style

--- src/components/Map/Source/osm.js
+++ src/components/Map/Source/osm.js
@@ -1,7 +1,10 @@
 import * as olSource from "ol/source";
+import { mapBoxUrl } from "./credentials";
 
 function osm() {
-	return new olSource.OSM();
+	return new olSource.OSM({
+		url: mapBoxUrl,
+	});
 }
 
 export default osm;
